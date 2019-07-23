33761380426ddfbb2eff4cdc9ed87988b6f621b6
Nicolaj Filrup Rasmussen <nixen97@gmail.com>
2019-07-23 14:41:48 +0200

    Better entrypoint

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,8 +5,4 @@ if [ ! -f "/etc/ssl/certs/dhparam.pem" ]; then
     echo "Generated Diffie-Helmman key"
 fi
 
-if [ -f "/usr/sbin/nginx" ]; then
-    /usr/sbin/nginx -f "daemon off;"
-else
-    echo "Somethings up: Can't find nginx"
-fi
+nginx -g "daemon off;"
